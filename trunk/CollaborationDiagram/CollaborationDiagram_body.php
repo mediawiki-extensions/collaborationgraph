fea1a5b61307a8fe34f1b5aa1075cc2ad2c5cb35
ganqTurgon <ganqTurgon@5b20f252-f2f9-4ffa-04b9-da9e9a85f618>
2010-11-21 22:51:12 +0000

    initial version

---
+++ trunk/CollaborationDiagram/CollaborationDiagram_body.php
@@ -0,0 +1,22 @@
+<?php
+class CollaborationDiagram extends SpecialPage {
+  function __construct() {
+    parent::__construct( 'CollaborationDiagram' );
+    wfLoadExtensionMessages('CollaborationDiagram');
+  }
+
+  function execute( $par ) {
+    global $wgRequest, $wgOut;
+
+    $this->setHeaders();
+
+    # Get request data from, e.g.
+    $param = $wgRequest->getText('param');
+
+    # Do stuff
+    # ...
+    $output="Hello world!";
+    $wgOut->addWikiText( $output );
+  }
+}
+
