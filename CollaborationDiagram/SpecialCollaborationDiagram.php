7ef4975e297ef0c81d3b7603e956f96218bf6906
ganqTurgon <ganqTurgon@5b20f252-f2f9-4ffa-04b9-da9e9a85f618>
2010-11-23 00:01:15 +0000

    multiple pages in page parameter

--- CollaborationDiagram/SpecialCollaborationDiagram.php
+++ CollaborationDiagram/SpecialCollaborationDiagram.php
@@ -8,19 +8,11 @@ EOT;
   exit( 1 );
 }
  
-$wgExtensionCredits['specialpage'][] = array(
-  'name' => 'CollaborationDiagram',
-  'author' => 'Yury Katkov',
-  'url' => 'http://www.mediawiki.org/wiki/Extension:CollaborationDiagram',
-  'description' => 'Show graph that represent how much each user participated in a creation of the article',
-  'descriptionmsg' => 'CollaborationDiagram-desc',
-  'version' => '0.0.0',
-);
  
 $dir = dirname(__FILE__) . '/';
  
 $wgAutoloadClasses['CollaborationDiagram'] = $dir . 'CollaborationDiagram_body.php'; # Location of the CollaborationDiagram class (Tell MediaWiki to load this file)
 //$wgExtensionMessagesFiles['CollaborationDiagram'] = $dir . 'CollaborationDiagram.i18n.php'; # Location of a messages file (Tell MediaWiki to load this file)
-//$wgExtensionAliasesFiles['CollaborationDiagram'] = $dir . 'CollaborationDiagram.alias.php'; # Location of an alias file (Tell MediaWiki to load this file)
+$wgExtensionAliasesFiles['CollaborationDiagram'] = $dir . 'CollaborationDiagram.alias.php'; # Location of an alias file (Tell MediaWiki to load this file)
 $wgSpecialPages['CollaborationDiagram'] = 'CollaborationDiagram'; # Tell MediaWiki about the new special page and its class name
 
