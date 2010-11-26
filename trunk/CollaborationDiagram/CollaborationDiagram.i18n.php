3941ba788ba3e89a19f557c0a7bb3703f59d09c2
ganqTurgon <ganqTurgon@5b20f252-f2f9-4ffa-04b9-da9e9a85f618>
2010-11-26 19:45:56 +0000

    added support for piecharts

---
+++ trunk/CollaborationDiagram/CollaborationDiagram.i18n.php
@@ -0,0 +1,17 @@
+<?php
+$messages = array();
+ 
+/* *** English *** */
+$messages['en'] = array(
+    'collaborationdiagram' => 'Collaboration Diagram',
+      'collaborationdiagram-desc' => "Shows collaboration graph",
+    );
+ 
+/* *** German (Deutsch) *** */
+$messages['ru'] = array(
+    'collaborationdiagram' => 'Диаграмма соучастия',
+      'myextension-desc' => 'Показывает граф соучастия',
+    );
+
+
+
