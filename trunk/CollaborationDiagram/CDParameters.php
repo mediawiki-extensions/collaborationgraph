15f0bf17a8be34d9a97e2a26b1aae0c041646ccc
ganqTurgon <ganqTurgon@5b20f252-f2f9-4ffa-04b9-da9e9a85f618>
2011-03-28 16:33:07 +0000

    tag paramenters are now in a separate class

--- trunk/CollaborationDiagram/CDParameters.php
+++ trunk/CollaborationDiagram/CDParameters.php
@@ -8,6 +8,33 @@ EOT;
   exit( 1 );
 }
 
+function getCategoryPagesFromDb($categoryName)
+{
+  global $wgDBprefix;
+  //and go!
+  $dbr =& wfGetDB( DB_SLAVE );
+  $tbl_categoryLinks = $wgDBprefix.'categorylinks';
+  $pageTable = $wgDBprefix.'page';
+  $categoryName = mysql_real_escape_string($categoryName);
+  $sql = "
+    SELECT
+    page_title
+    from $tbl_categoryLinks 
+    LEFT JOIN $pageTable on $tbl_categoryLinks.cl_from=$pageTable.page_id
+    WHERE cl_to=\"$categoryName\";
+  ";
+  $res = $dbr->query($sql);
+
+  $result = array();
+  //formatting output array of names:
+  foreach ($res as $row)
+  {
+    array_push($result, $row->page_title);
+  }
+  return $result;
+}
+
+
 class CDParameters {
   private $skin;
   private $pagesList;
@@ -29,6 +56,53 @@ class CDParameters {
   private function __clone() {
   }
 
-  public function setup($args) {
+  public function setup(array $args) {
+    global $wgRequest, $wgCollaborationDiagramSkinFilename, $wgOut;
+    $this->pagesList = array();
+    if (!isset($args["page"])&&!isset($args['category']))
+    {
+      $this->pagesList = array($wgRequest->getText('title'));
+    }
+
+    if  (isset($args["page"]))
+    {
+      $this->pagesList = explode(";",$args["page"]);
+    }
+
+    if (isset($args["category"]))
+    {
+      $pagesFromCategory = array();
+      $pagesFromCategory = getCategoryPagesFromDb($args["category"]);
+      $this->pagesList = array_merge($this->pagesList, $pagesFromCategory) ;
+      $this->category=$args['category'];//XXX
+    }
+
+    $this->skin = 'default.dot';
+    if (isset($wgCollaborationDiagramSkinFilename))
+    {
+      $this->skin = $wgCollaborationDiagramSkinFilename;
+    }
+
+    $this->diagramType = 'dot';
+    if (isset($args['type']))
+    {
+      $this->diagramType= $args['type'];
+    }
+  }
+
+
+  public function getSkin() {
+    return $this->skin;        
+
+  }
+
+  public function getPagesList() {
+    return $this->pagesList;   
+  }
+  public function getCategory() {
+    return $this->category;    
+  }
+  public function getDiagramType() {
+    return $this->diagramType; 
   }
 }
